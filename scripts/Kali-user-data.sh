8c4863e824666e9faf0f14fcca64bea17d25d924
Mike Lim <88474310+limmike@users.noreply.github.com>
2023-07-08 07:41:52 +0800

    Fix NICE DCV install error

--- scripts/Kali-user-data.sh
+++ scripts/Kali-user-data.sh
@@ -1,54 +1,52 @@
 #!/bin/zsh
 cd /root/
 
-# https://stackoverflow.com/questions/33370297/apt-get-update-non-interactive
-export DEBIAN_FRONTEND=noninteractive   
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
 
 # https://docs.aws.amazon.com/systems-manager/latest/userguide/agent-install-ubuntu.html
-apt-get install -y snapd
-systemctl enable --now snapd
+apt-get install -q -y snapd
+systemctl enable snapd
 systemctl start snapd
 sleep 2
 snap install amazon-ssm-agent --classic
 snap start amazon-ssm-agent
 
-# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/releasehistory-aws-cfn-bootstrap.html#releasehistory-aws-cfn-bootstrap-v1
-apt-get -q -y install python3-setuptools wget tmux unzip tar curl sed
-
+apt-get install -q -y wget tmux unzip tar curl sed
 cd /root/
+
 apt-get -q -y install kali-desktop-xfce
-apt-get -q -y install pulseaudio-utils 
+apt-get -q -y install pulseaudio-utils
 
 # https://docs.aws.amazon.com/dcv/latest/adminguide/setting-up-installing-linux-server.html
 wget -nv https://d1uj6qtbmh3dt5.cloudfront.net/NICE-GPG-KEY
 gpg --import NICE-GPG-KEY
-wget -nv https://d1uj6qtbmh3dt5.cloudfront.net/nice-dcv-ubuntu2004-x86_64.tgz
-tar -xvzf nice-dcv-ubuntu2004-x86_64.tgz && cd nice-dcv-*-ubuntu2004-x86_64
+wget -nv https://d1uj6qtbmh3dt5.cloudfront.net/nice-dcv-ubuntu2204-x86_64.tgz
+tar -xvzf nice-dcv-ubuntu2204-x86_64.tgz && cd nice-dcv-*-ubuntu2204-x86_64
 
 # tweaks for installation on Kali
 ln -s /etc/os-release /etc/lsb-release
 mkdir -p /etc/lightdm/lightdm.conf.d
 
 # https://docs.aws.amazon.com/dcv/latest/adminguide/setting-up-installing-linux-server.html#linux-server-install
-apt-get -q -y install ./nice-dcv-server_*_amd64.ubuntu2004.deb
-apt-get -q -y install ./nice-dcv-web-viewer_*_amd64.ubuntu2004.deb
+apt-get -q -y install ./nice-dcv-server_*_amd64.ubuntu2204.deb
+apt-get -q -y install ./nice-dcv-web-viewer_*_amd64.ubuntu2204.deb
 usermod -aG video dcv 
 # from /etc/lightdm/lightdm.conf.d
 sed -i '/^\[Seat\:\*\]/a display-setup-script=/usr/lib/x86_64-linux-gnu/dcv/dcvlightdm' /etc/lightdm/lightdm.conf
 
 # virtual session support
-apt-get -q -y install ./nice-xdcv_*_amd64.ubuntu2004.deb
+apt-get -q -y install ./nice-xdcv_*_amd64.ubuntu2204.deb
 
 # https://docs.aws.amazon.com/dcv/latest/adminguide/enable-quic.html
 cp /etc/dcv/dcv.conf /etc/dcv/dcv.conf.org
-sed -i '/^\[connectivity/a enable-quic-frontend=true' /etc/dcv/dcv.conf
+sed -i "s/^#enable-quic-frontend=true/enable-quic-frontend=true/g" /etc/dcv/dcv.conf
 
 # session storage: https://docs.aws.amazon.com/dcv/latest/userguide/using-transfer.html
 # https://docs.aws.amazon.com/dcv/latest/adminguide/managing-sessions-start.html#managing-sessions-start-manual
 cat << EoF > /etc/systemd/system/dcv-virtual-session.service
 [Unit]
-Description=Create DCV virtual session for user kali
+Description=Create DCV virtual session
 After=default.target network.target 
 
 [Service]
@@ -84,6 +82,47 @@ curl -s https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
 unzip -q -o awscliv2.zip
 ./aws/install -b /usr/bin
 echo "export AWS_CLI_AUTO_PROMPT=on-partial" >> /home/kali/.zshrc
+
+# DCV update script
+cat << EoF > /home/kali/update-dcv
+#!/bin/zsh
+cd /tmp
+rm -f /tmp/nice-dcv-ubuntu2204-x86_64.tgz
+wget https://d1uj6qtbmh3dt5.cloudfront.net/nice-dcv-ubuntu2204-x86_64.tgz
+tar -xvzf nice-dcv-ubuntu2204-x86_64.tgz && cd nice-dcv-*-ubuntu2204-x86_64
+sudo dcv close-session kali
+sudo systemctl stop dcvserver dcv-virtual-session
+sudo apt-get install -y ./nice-dcv-server_*_amd64.ubuntu2204.deb 
+sudo apt-get install -y ./nice-dcv-web-viewer_*_amd64.ubuntu2204.deb 
+sudo apt-get install -y ./nice-xdcv_*_amd64.ubuntu2204.deb 
+sudo sed -i "s/^#enable-quic-frontend=true/enable-quic-frontend=true/g" /etc/dcv/dcv.conf
+sudo systemctl restart dcvserver dcv-virtual-session
+EoF
+chmod +x /home/kali/update-dcv
+chown kali:kali /home/kali/update-dcv 
+
+# AWS CLI update script
+cat << EoF > /home/kali/update-awscli
+#!/bin/zsh
+cd /tmp
+rm -f awscliv2.zip
+curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
+unzip -q -o awscliv2.zip
+sudo ./aws/install --update -b /usr/bin
+EoF
+chmod +x /home/kali/update-awscli
+chown kali:kali /home/kali/update-awscli   
+
+
+# Fix "Authentication Required to Create Managed Color Device" prompt
+cat << EoF > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EoF
 
 # text console: DCV virtual sessions only
 systemctl isolate multi-user.target
